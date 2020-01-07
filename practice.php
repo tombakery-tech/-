ad1e5655373d37afa9a158bcfe1888f39b052fd3
kato <itizisiyouitasimasu@hotmail.co.jp>
2020-01-07 07:03:37 +0000

    課題提出

---
+++ practice.php
@@ -0,0 +1,30 @@
+<?php
+$a = 3;
+$b = 7;
+echo $a+$b ;
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array_month[7];
+$hello = "Heloo,";
+$name = "kato k";
+$world = "'s World";
+echo $hello ;
+echo $name ;
+echo $world ;
+$tech_boost = "tech";
+echo $tech_boost . "boost";
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+// 12月を表示する
+echo $calendar_2018["December"];
