136cc822cda7f06902d2d765831dade21e800909
kato <itizisiyouitasimasu@hotmail.co.jp>
2020-01-16 08:18:34 +0000

    課題提出4

---
+++ ProfileController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+    public function edit()
+    {
+        return view('admin.profile.create');
+    }
+    public function update()
+    {
+         return redirect('admin/profile/create');
+    }
+}
+
